6ca2b411b8828107de5837ae27a0d900485b7b15
Angie <angiewashburn1997@gmail.com>
2016-09-19 08:03:37 -0400

    Add project files.

---
+++ ShoppingListVersion2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoppingListVersion2.MvcApplication" Language="C#" %>
---
+++ ShoppingListVersion2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingListVersion2.Startup))]
+namespace ShoppingListVersion2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
